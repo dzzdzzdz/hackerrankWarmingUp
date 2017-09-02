0539b6ec6b8652a3a3854b74795c3b25d5276f24
dzzdzzdz <danielzhaozd@gmail.com>
2017-09-02 15:00:24 -0400

    test

---
+++ elementsOfProgrammingInterviews/CPP/ch05_primitiveTypes/computingParity.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+short parity1(unsigned long x) {
+  short result = 0;
+  while (x) {
+    result ^= (x & 1);
+    x >>= 1;
+  }
+  return result;
+}
+
+int main() {
+  cout <<  parity1(15) << endl;
+  return 0;
+}
